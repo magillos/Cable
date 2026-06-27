9ab9ec64fe0d3abe869b0d5262f72abcce29dbd7
magillos <kerown@gmail.com>
2026-06-27 22:23:17 +0100

    #56

--- cable_core/app_config.py
+++ cable_core/app_config.py
@@ -10,7 +10,7 @@ from typing import Optional
 from PyQt6.QtGui import QIcon
 
 # --- Application Version ---
-APP_VERSION = "0.10.11"
+APP_VERSION = "0.10.12"
 
 # --- Shared Constants ---
 EDIT_LIST_TEXT = "Edit List..."
